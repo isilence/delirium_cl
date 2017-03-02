3836ae9fba9f6b2bdda65b85ced89962a01e59b6
sil <asml.silence@gmail.com>
2017-03-02 22:17:29 +0300

    Add runKernel support for MemobjPool

--- inc/dlm/cl/kernel.hpp
+++ inc/dlm/cl/kernel.hpp
@@ -2,58 +2,97 @@
 #define DLM_CL_KERNEL_HPP_
 #pragma once
 #include "dlm/cl/base.hpp"
+#include "dlm/cl/memobjPool.hpp"
 
 namespace dlmcl {
 
-struct Void {};
+static inline cl_int setKernelArguments(cl_command_queue DLM_UNUSED(queue),
+                                        cl_kernel DLM_UNUSED(kernel),
+                                        size_t DLM_UNUSED(num))
+{
+    return CL_SUCCESS;
+}
 
 template <typename T, typename ...Args>
-inline
-cl_int setKernelArguments(  cl_command_queue queue,
-                            cl_kernel kernel,
-                            size_t num,
-                            T param,
-                            Args... xs)
+static inline cl_int setKernelArguments(cl_command_queue queue,
+                                        cl_kernel kernel,
+                                        cl_uint num,
+                                        T param,
+                                        Args... xs)
 {
-    const cl_int error = clSetKernelArg( kernel, num, sizeof(param), (void*)&param);
+    const cl_int error = clSetKernelArg(kernel, num, sizeof(param), (void*)&param);
     if (error != CL_SUCCESS)
         return error;
     return setKernelArguments(queue, kernel, num+1, xs...);
 }
 
 template <typename ...Args>
-inline
-cl_int setKernelArguments(  cl_command_queue queue,
-                            cl_kernel kernel,
-                            size_t num,
-                            Memobj* mem,
-                            Args... xs)
+static inline cl_int setKernelArguments(cl_command_queue queue,
+                                        cl_kernel kernel,
+                                        cl_uint num,
+                                        Memobj* mem,
+                                        Args... xs)
 {
     mem->switchContext(queue, Memobj::MCT_DEVICE);
+    mem->switchToDevice(queue);
     cl_mem clMem = mem->getDeviceMemory();
     return setKernelArguments(queue, kernel, num, clMem, xs...);
 }
 
-template <>
-inline
-cl_int setKernelArguments<Void>(cl_command_queue DLM_UNUSED(queue),
-                                cl_kernel DLM_UNUSED(kernel),
-                                size_t DLM_UNUSED(num),
-                                Void DLM_UNUSED(stub))
+template <typename ...Args>
+static inline cl_int setKernelArguments(cl_command_queue queue,
+                                        cl_kernel kernel,
+                                        cl_uint num,
+                                        MemobjPool& pool,
+                                        Memobj* mem,
+                                        Args... xs)
 {
-    return CL_SUCCESS;
+    mem->switchContext(queue, Memobj::MCT_DEVICE);
+    mem->switchToDevice(queue);
+    cl_mem clMem = mem->getDeviceMemory();
+    return setKernelArguments(queue, kernel, num, pool, clMem, xs...);
+}
+
+template <typename T, typename ...Args>
+static inline cl_int setKernelArguments(cl_command_queue queue,
+                                        cl_kernel kernel,
+                                        cl_uint num,
+                                        MemobjPool& pool,
+                                        T* param,
+                                        Args... xs)
+{
+    Memobj* const ptr = pool.get((void*)param);
+    if (ptr == nullptr)
+        return CL_MEM_OBJECT_ALLOCATION_FAILURE;
+
+    return setKernelArguments(queue, kernel, num, pool, ptr, xs...);
+}
+
+
+template<typename ...Args>
+static inline cl_int runKernel( cl_kernel kernel,
+                                cl_command_queue queue,
+                                const cl_uint dimSize,
+                                const size_t globalSize[],
+                                const size_t localSize[],
+                                Args... xs)
+{
+    cl_int error = setKernelArguments(queue, kernel, 0, xs...);
+    if (error != CL_SUCCESS)
+        return error;
+    return clEnqueueNDRangeKernel(queue, kernel, dimSize, NULL, globalSize, localSize, 0, NULL, NULL);
 }
 
 template<typename ...Args>
-inline
-cl_int runKernel(   cl_kernel kernel,
-                    cl_command_queue queue,
-                    const size_t dimSize,
-                    const size_t globalSize[],
-                    const size_t localSize[],
-                    Args... xs)
+static inline cl_int runKernel( cl_kernel kernel,
+                                cl_command_queue queue,
+                                const cl_uint dimSize,
+                                const size_t globalSize[],
+                                const size_t localSize[],
+                                MemobjPool& pool,
+                                Args... xs)
 {
-    cl_int error = setKernelArguments(queue, kernel, 0, xs..., Void());
+    cl_int error = setKernelArguments(queue, kernel, 0, pool, xs...);
     if (error != CL_SUCCESS)
         return error;
     return clEnqueueNDRangeKernel(queue, kernel, dimSize, NULL, globalSize, localSize, 0, NULL, NULL);
